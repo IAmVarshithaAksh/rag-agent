4eaec2f897387127c9b939654f9413064a9b81bf
VarshithaAksh <varshithaakshaypatil@gmail.com>
2026-03-12 23:20:51 -0400

    Setup of three components such as chat model, embeddings, vectorbase

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
+import getpass
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import faiss
+from langchain_community.docstore.in_memory import InMemoryDocstore
+from langchain_community.vectorstores import FAISS
+
+
+os.environ["LANGSMITH_TRACING"] = "true"
+os.environ["LANGSMITH_API_KEY"] = getpass.getpass()
+os.environ["ANTHROPIC_API_KEY"] = "sk-..."
+if not os.environ.get("GOOGLE_API_KEY"):
+    os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter API key for Google Gemini: ")
+
+model = ChatAnthropic(model="claude-sonnet-4-6")
+
+embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
+
+embedding_dim = len(embeddings.embed_query("hello world"))
+index = faiss.IndexFlatL2(embedding_dim)
+
+vector_store = FAISS(
+    embedding_function=embeddings,
+    index=index,
+    docstore=InMemoryDocstore(),
+    index_to_docstore_id={},
+)
+
 def main():
     print("Hello from rag-agent!")
 
